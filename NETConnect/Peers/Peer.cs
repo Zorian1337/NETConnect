ba1594bf741b39e6b2c9d4340d3a2b4af4c9bbaa
Zorian1337 <SympathyGaming1337@gmail.com>
2026-02-23 06:05:19 -0500

    Added a peer class, that will handle all of our server/client needs, full functionality will be complete at a later time.

---
+++ NETConnect/Peers/Peer.cs
@@ -0,0 +1,40 @@
+﻿using NETConnect.Shared.Multicast;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETConnect.Peers;
+
+public class Peer
+{
+    // So each peer is a server and a client
+
+    // Locally clients will use udp multicast groups to look for groups
+
+    public BaseTCPClient TCPClient { get; set; }
+    public BaseTCPServer TCPCServer { get; set; }
+    public Multicast Multicast { get; set; }
+
+
+    public Peer()
+    {
+        // Create multicast to work immediately
+        Multicast = new Multicast();
+        Multicast.ReadMulticast();
+    }
+
+
+    //public void Start()
+    //{
+
+
+    //    // Host a server on the local port 
+
+    //    //
+    //}
+
+
+}
+
