865e021f8a3354728cbf8c37127e6016f4c8a7ce
Zorian1337 <SympathyGaming1337@gmail.com>
2026-02-23 22:37:20 -0500

    Added RSA class and extensions

---
+++ NETConnect/Encryption/Crypt/RSA.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Security.Cryptography;
+
+namespace NETConnect.Encryption.Crypt;
+
+
+public enum RSAKeySize : int
+{
+    Weak = 1024,
+    Minium = 2048,
+    Default = 3072,
+    HighSecurity = 4096,
+    VerySecure = 8192
+}
+
+
+
+
+public class RSACrypt
+{
+    public record RSAExport(byte[] PrivateKey, byte[] PublicKey);
+
+    public static RSA Create(RSAKeySize SecurityLevel = RSAKeySize.Default) => RSA.Create((int)SecurityLevel);
+    public static RSAParameters CreateParams(RSAKeySize SecurityLevel = RSAKeySize.Default)
+    {
+        using RSA rsa = RSA.Create((int)SecurityLevel);
+
+        RSAParameters Params = rsa.ExportParameters(true);
+
+        return Params;
+    }
+
+    public static RSAExport CreateExport(RSAKeySize SecurityLevel = RSAKeySize.Default) => CreateExport((int)SecurityLevel);
+    public static RSAExport CreateExport(int KeySize)
+    {
+        using RSA rsa = RSA.Create(KeySize);
+        return new RSAExport(rsa.ExportPkcs8PrivateKey(), rsa.ExportSubjectPublicKeyInfo());
+    }
+
+    public static string PemEncode(string label, byte[] data)
+    {
+        var builder = new StringBuilder();
+        builder.AppendLine($"-----BEGIN {label}-----");
+        builder.AppendLine(Convert.ToBase64String(data, Base64FormattingOptions.InsertLineBreaks));
+        builder.AppendLine($"-----END {label}-----");
+        return builder.ToString();
+    }
+
+
+}
---
+++ NETConnect/MyExtensions/Encryption/RSAExtensions.cs
@@ -0,0 +1,82 @@
+﻿
+using NETConnect.Encryption.Crypt;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static NETConnect.Encryption.Crypt.RSACrypt;
+namespace NETConnect.MyExtensions.Encryption;
+
+public static class RSAExtensions
+{
+
+    public static RSAKeySize GetRSASecurityLevel(this int KeySize)
+    {
+        if (Enum.IsDefined(typeof(RSAKeySize), KeySize)) return (RSAKeySize)KeySize;
+        else return default; // this'll need fixed later
+    }
+
+    public static RSAKeySize GetRSASecurityLevel(this byte[] RSAPubKey)
+    {
+        using RSA rsa = RSA.Create();
+        rsa.ImportSubjectPublicKeyInfo(RSAPubKey, out _);
+
+        return GetRSASecurityLevel(rsa.KeySize);
+    }
+
+    public static byte[] EncryptRSA(this byte[] Data, byte[] PublicKey)
+    {
+        try
+        {
+            using RSA rsa = RSA.Create();
+            rsa.ImportSubjectPublicKeyInfo(PublicKey, out _);
+
+            RSAEncryptionPadding Padding;
+            switch (rsa.KeySize.GetRSASecurityLevel())
+            {
+                case RSAKeySize.Weak: Padding = RSAEncryptionPadding.OaepSHA1; break;
+                default: Padding = RSAEncryptionPadding.OaepSHA3_256; break;
+            }
+
+            return rsa.Encrypt(Data, Padding);
+        }
+        catch (Exception Ex) { Console.WriteLine(Ex.ToString()); Debug.WriteLine(Ex.ToString()); }
+        
+        return Array.Empty<byte>();
+    }
+
+    public static byte[] DecryptRSA(this byte[] Data, byte[] PrivateKey)
+    {
+        try
+        {
+            using RSA rsa = RSA.Create();
+            rsa.ImportPkcs8PrivateKey(PrivateKey, out _);
+            //rsa.ImportSubjectPublicKeyInfo(PublicKey, out _);
+
+            RSAEncryptionPadding Padding;
+            switch (rsa.KeySize.GetRSASecurityLevel())
+            {
+                case RSAKeySize.Weak: Padding = RSAEncryptionPadding.OaepSHA1; break;
+                default: Padding = RSAEncryptionPadding.OaepSHA3_256; break;
+            }
+
+            return rsa.Decrypt(Data, Padding);
+        }
+        catch (Exception Ex) { Console.WriteLine(Ex.ToString()); Debug.WriteLine(Ex.ToString()); }
+
+        return Array.Empty<byte>();
+    }
+
+
+    public static RSAExport GetRSAKeys(this RSA Crypt)
+    {
+        return new RSAExport(Crypt.ExportPkcs8PrivateKey(), Crypt.ExportSubjectPublicKeyInfo());
+    }
+
+
+}
