951cca3ae8054526174e820a72dd4e8d7f87de37
Zorian1337 <SympathyGaming1337@gmail.com>
2026-02-27 21:14:03 -0500

    added class related to tracking when the peer comes online and when it goes offline to track average uptime and use it for availability checks

---
+++ NETConnect/Network/Info/UptimeTracker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETConnect.Network.Info
+{
+    /// <summary>
+    /// Uses this data to form a collective average uptime using collections of this data
+    /// </summary>
+    public class UptimeTracker
+    {
+        public DateTime OnlineAt { get; set; }
+        public DateTime OfflineAt { get; set; }
+    }
+}
