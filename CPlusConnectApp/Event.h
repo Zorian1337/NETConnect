877389d432c5d8b47db351f5cc74d48b3a6bf575
Zorian1337 <SympathyGaming1337@gmail.com>
2026-03-12 01:40:39 -0400

    Created Event header to create a system familiar to c# events

---
+++ CPlusConnectApp/Event.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <algorithm> // for std::remove
+
+
+// Generic Event class
+template <typename... Args>
+class Event
+{
+public:
+	using Callback = std::function<void(Args...)>;
+
+	// Removed for C# style 
+	//void subscribe(std::function<void(Args...)> callback) {
+	//	listeners.push_back(callback);
+	//}
+
+	void operator+=(Callback cb) {
+		listeners.push_back(cb);
+	}
+
+	// Doesnt support removal of lambdas
+	void operator-=(Callback cb) {
+		listeners.erase(
+			std::remove_if(listeners.begin(), listeners.end(),
+				[&](const Callback& f) {
+					return f.target_type() == cb.target_type() &&
+						f.target<void(*)(Args...)>() == cb.target<void(*)(Args...)>();
+				}),
+			listeners.end()
+		);
+	}
+
+	void Invoke(Args... args) {
+		for (auto& cb : listeners)
+			cb(args...);
+	}
+private:
+	std::vector<std::function<void(Args...)>> listeners;
+};
+
