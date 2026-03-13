c4a5a0d8b9150db89531f9763c82b681856fb6f8
Zorian1337 <SympathyGaming1337@gmail.com>
2026-03-13 03:34:49 -0400

    Added Console.ReadKey() for display purposes (probably removing later just wanted to push to branch)

--- CPlusConnectApp/main.cpp
+++ CPlusConnectApp/main.cpp
@@ -11,7 +11,7 @@ int main() {
 
 	//std::cout << "Hello World" << std::endl;
 
-	printf("C++ CPPConnect\n");
+	printf("C++ NETConnect\n");
 
 	std::cin.get();
 
--- NETConnectAPP/Program.cs
+++ NETConnectAPP/Program.cs
@@ -33,38 +33,6 @@ namespace NETConnectAPP
                 //var Helper = Client.Packer;
                 //NETConnect.Audio.Audio.StartStreaming(ref Helper);
                 Thread.Sleep(1000);
-
-
-
-                //ConsoleKeyInfo Key = Console.ReadKey(true);
-                //if (Key.Key == ConsoleKey.Tab)
-                //{
-                //    //// Take current Top Location
-                //    //int CurrentTop = Console.CursorTop;
-                    
-                //    //if (Console.CursorLeft != 0)
-                //    //{
-                //    //    Console.CursorLeft = 0;
-                //    //    Console.CursorTop = CurrentTop + 1;
-                //    //}
-
-                //    //int NewTop = Console.CursorTop;
-                //    //int NewLeft = Console.CursorLeft;
-                //    //Console.Write("Your Message: ");
-
-                //    ConsoleDebugging.Print(new ConsoleDebugging.ConsoleBufferItem(Console.BufferHeight - 1, Console.CursorLeft, "Your Message: ", ConsoleDebugging.ConsoleBufferReturnPosition.NewPosition, () =>
-                //    {
-                //        string Type = Console.ReadLine();
-                //        ConsoleDebugging.Print(Type);
-                //    }));
-
-                //    string Message = Console.ReadLine();
-                //    peer.Multicast.SendUTF8Message(Message, MulticastAction.Data);
-                //}
-
-
-
-                //peer.Multicast.SendMessage(Hash, MulticastAction.Data);
             }
 
 
