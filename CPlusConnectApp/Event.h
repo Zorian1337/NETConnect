6a5cda8a2347709db01212bade1f4c9ee7042f9f
Zorian1337 <SympathyGaming1337@gmail.com>
2026-03-15 02:45:36 -0400

    Created a threadPool to get the most out of our limited threads Added support for threadPool to Events with the InvokeAsync

--- CPlusConnectApp/Event.h
+++ CPlusConnectApp/Event.h
@@ -4,7 +4,9 @@
 #include <vector>
 #include <functional>
 #include <algorithm> // for std::remove
-
+#include <mutex>  // Helps with thread safety
+#include <thread> // Added for async
+#include "ThreadPool.h"
 
 // Generic Event class
 template <typename... Args>
@@ -13,17 +15,19 @@ class Event
 public:
 	using Callback = std::function<void(Args...)>;
 
-	// Removed for C# style 
-	//void subscribe(std::function<void(Args...)> callback) {
-	//	listeners.push_back(callback);
-	//}
+	Event(ThreadPool& pool) : threadPool(pool) {}
 
+	Event() : threadPool(nullptr) {}
+
+	// Thread safe
 	void operator+=(Callback cb) {
+		std::lock_guard<std::mutex> lock(mutex);
 		listeners.push_back(cb);
 	}
 
 	template<typename T>
 	void Subscribe(T* instance, void (T::* memberFunction)(Args...)) {
+		std::lock_guard<std::mutex> lock(mutex);
 		listeners.push_back([instance, memberFunction](Args... args) {
 			(instance->*memberFunction)(args...);
 			});
@@ -31,6 +35,7 @@ public:
 
 	// Doesnt support removal of lambdas
 	void operator-=(Callback cb) {
+		std::lock_guard<std::mutex> lock(mutex);
 		listeners.erase(
 			std::remove_if(listeners.begin(), listeners.end(),
 				[&](const Callback& f) {
@@ -41,11 +46,44 @@ public:
 		);
 	}
 
+	// Runs in sync
 	void Invoke(Args... args) {
-		for (auto& cb : listeners)
+		std::vector<Callback> currentListeners;
+		{
+			std::lock_guard<std::mutex> lock(mutex); 
+			currentListeners = listeners;
+		}
+
+		for (auto& cb : currentListeners)
 			cb(args...);
 	}
+
+	// Runs Async
+    void InvokeAsync(Args... args) 
+	{
+		std::vector<Callback> currentListeners;
+		{
+			std::lock_guard<std::mutex> lock(mutex); 
+			currentListeners = listeners;
+		}
+
+        for (auto& cb : currentListeners) {
+
+			// Uses thread pools 
+			threadPool->enqueue([cb, args...]() {
+				cb(args...);
+				});
+
+            // Launch each listener in its own thread
+            //std::thread([cb, args...]() {
+            //    cb(args...);
+            //}).detach();
+		}
+	}
+	
 private:
 	std::vector<std::function<void(Args...)>> listeners;
+	std::mutex mutex; // Mutex to protect listeners vector
+	ThreadPool* threadPool;; // Reference to the thread pool for async invocation
 };
 
