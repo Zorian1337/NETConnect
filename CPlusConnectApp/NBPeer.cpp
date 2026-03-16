07d9c03cec99ce9fcd8485eb245974a63ff5a5da
Zorian1337 <SympathyGaming1337@gmail.com>
2026-03-16 04:09:16 -0400

    Add ThreadPool support to NBPeer for async operations
    - Replace manual thread management with ThreadPool
    - Enable non-blocking server/client operations
    - Improve performance by reusing threads
    - Fix thread cleanup in destructor to prevent crashes

--- CPlusConnectApp/NBPeer.cpp
+++ CPlusConnectApp/NBPeer.cpp
@@ -1,38 +1,11 @@
 #include "NBPeer.h"
 
 
+//NBPeer::NBPeer() : threadPool(std::thread::hardware_concurrency()), Peer()
+//{
+//
+//}
 
-NBPeer::NBPeer() : threadPool(std::thread::hardware_concurrency()), Peer()
-{
-
-}
-
-void NBPeer::Start() {
-	mainThread = std::thread([this]() {
-		// This is where we will start our Node and all the servers and stuff, but for now we will just print a message to indicate it started.
-		printf("NBPeer started!\n");
-
-		while (IsMainThreadRunning) 
-		{
-			// Start our servers without listeners
-
-			if (!Peer.LAN.IsServerRunning) {
-				// Start our multicast server if its not already
-				Peer.LAN.StartMulticastServer("235.69.4.20", 50420, false);
-			}
-
-			// Starting with our Multicast for now
-			//if (!Peer.TServer.IsServerRunning) {
-			//	// Start our tcp server if its not already
-			//	Peer.TServer.StartServer(0, false);
-			//}
-			
-
-			// Check our servers for new data.
-
-			Peer.LAN.Server.sock.rec
-
-			std::this_thread::sleep_for(std::chrono::microseconds(100));
-		}
-	});
-}
+//void NBPeer::Start() {
+//
+//}
--- CPlusConnectApp/NBPeer.h
+++ CPlusConnectApp/NBPeer.h
@@ -7,24 +7,64 @@
 
 #include "Node.h"
 #include "ThreadPool.h"
+#include "UTF8Helper.h"
 
 class NBPeer
 {
 private:
 	std::thread mainThread;
-
+	ThreadPool threadPool;
 
 public:
-	std::atomic<bool> IsMainThreadRunning { true };
 
-	NBPeer();
+	explicit NBPeer() : threadPool(std::thread::hardware_concurrency()), IsMainThreadRunning(true), Peer(new Node()) {}
+	std::atomic<bool> IsMainThreadRunning;// { true };
 
 	// Container of all our peers
-	Node Peer;
-	ThreadPool threadPool;
+	Node* Peer;// = new Node();
 	
+	~NBPeer() {
+		IsMainThreadRunning = false;
+
+		if (mainThread.joinable()) {
+			mainThread.join();
+		}
+	}
+
+	void Start() {
+		mainThread = std::thread([this]() {
+			// This is where we will start our Node and all the servers and stuff, but for now we will just print a message to indicate it started.
+			printf("NBPeer started!\n");
 
 
-	void Start();
+			Debugger::WriteLine("Thread started, IsMainThreadRunning = " +
+				std::to_string(IsMainThreadRunning.load()));
+			while (IsMainThreadRunning)
+			{
+				// Start our servers without listeners
+
+				if (!Peer->LAN.IsServerRunning) {
+					// Start our multicast server if its not already
+					if (Peer->LAN.StartMulticastServer("235.69.4.20", 50420, false)) {
+						Debugger::WriteLine("Started multicast");
+					}
+				}
+
+				// Starting with our Multicast for now
+				if (!Peer->TServer.IsServerRunning) {
+					// Start our tcp server if its not already
+					if (Peer->TServer.StartServer(0, false)) {
+						Debugger::WriteLine("Started tcp-server");
+					}
+				}
+
+
+				// Check our servers for new data.
+
+				std::this_thread::sleep_for(std::chrono::microseconds(100));
+			}
+			Debugger::WriteLine("out of scope[main]");
+			});
+	}
 };
 
