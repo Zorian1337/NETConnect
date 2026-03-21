7702e3268c91708b6039e332899c0d9ebd7ac2d1
Zorian1337 <SympathyGaming1337@gmail.com>
2026-03-21 03:23:11 -0400

    Added direct way to create a json from our current class with its types automatically being cast directly for compatiblity with c#

--- CPlusConnectApp/include/UTF8Helper.h
+++ CPlusConnectApp/include/UTF8Helper.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include "json.hpp"
+#include <initializer_list>
 #include <sstream>
 
 class UTF8Helper
@@ -17,6 +18,29 @@ public:
 		return std::string(bytes.begin(), bytes.end());
 	}
 
+
+	struct KeyValuePair {
+		std::string key;
+		nlohmann::json value;
+
+		template <typename Value>
+		KeyValuePair(const std::string& k, const Value& v) : key(k), value(v) {}
+	};
+
+	static std::string ToJSON(std::initializer_list<KeyValuePair> pairs) {
+		nlohmann::ordered_json j;
+		for (const auto& pair : pairs) { j[pair.key] = pair.value; }
+			
+		// Example 
+		//std::string json = UTF8Helper::ToJSON({
+		//	{"PeerId", Peer.Peer->PeerId.str()},
+		//	{"PacketAction", static_cast<int>(PacketActionType::ACK)},
+		//	{"PacketAction2", static_cast<int>(PacketActionType::ACK)}
+		//});
+
+		return j.dump().c_str(); // Added c_str() as its included with others to make it print out right
+	}
+
 	static nlohmann::json ToParsedJSON(std::string& UTF8String) {
 		return nlohmann::json::parse(UTF8String);
 	}
@@ -24,8 +48,6 @@ public:
 	static nlohmann::json ToParsedJSON(const char* UTF8bytes, int length) {
 		return nlohmann::json::parse(UTF8Helper::ToString(UTF8bytes, length));
 	}
-
-
 
 	static std::vector<uint8_t> ToBytes(std::string& UTF8String) {
 		return std::vector<uint8_t>(UTF8String.begin(), UTF8String.end());
