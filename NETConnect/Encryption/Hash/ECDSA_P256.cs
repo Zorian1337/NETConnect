45114e0f7a324c378f0d1d73a5461924c7d6b2e6
Zorian1337 <SympathyGaming1337@gmail.com>
2026-08-18 01:02:31 -0400

    Added support for signature generation for our packets down the line; Will be added close to the end of the packet but excluding the things that will change like LastHopid and TTL per gossip

---
+++ NETConnect/Encryption/Hash/ECDSA_P256.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace NETConnect.Encryption.Hash
+{
+    public static class ECDSA_P256
+    {
+        // ExportPkcs8PrivateKey to be loaded from file later
+        public static ECDsa GenerateIdentityKey() => ECDsa.Create(ECCurve.NamedCurves.nistP256);
+        public static byte[] ExportPrivateKey(ECDsa Key) => Key.ExportPkcs8PrivateKey();
+        public static byte[] ExportPublicKey(ECDsa Key) => Key.ExportSubjectPublicKeyInfo();
+        public static ECDsa ImportPrivateKey(byte[] data)
+        {
+            ECDsa key = ECDsa.Create();
+            key.ImportPkcs8PrivateKey(data, out _);
+            return key;
+        }
+        public static byte[] GenerateSignature(ECDsa Key, byte[] Data, HashAlgorithmName HashType) => Key.SignData(Data, HashType);
+        public static bool IsValidSignature(ECDsa Key, byte[] Signature, byte[] Data, HashAlgorithmName HashType) => Key.VerifyData(Data, Signature, HashType); 
+
+    }
+}
