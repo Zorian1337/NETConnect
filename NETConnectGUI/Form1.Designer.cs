148b6541f12568caafa5bf18e9dbde05478a2e24
Zorian1337 <SympathyGaming1337@gmail.com>
2026-08-22 04:27:14 -0400

    NetConnectGUI is the testing form that I use to make it easire to tell who is connected where and testing features out like broadcasting, Gossip, Encryption and further features like voice, screensharing and more

---
+++ NETConnectGUI/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace NETConnectGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblPeerId = new Label();
+            lblServerIP = new Label();
+            rtbConsole = new RichTextBox();
+            txtMessage = new TextBox();
+            btnSend = new Button();
+            statusStrip1 = new StatusStrip();
+            StripServerStatus = new ToolStripStatusLabel();
+            StripPeerCount = new ToolStripStatusLabel();
+            listPeerView = new ListBox();
+            listDiscoveredPeers = new ListBox();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblPeerId
+            // 
+            lblPeerId.AutoSize = true;
+            lblPeerId.Location = new Point(341, 149);
+            lblPeerId.Name = "lblPeerId";
+            lblPeerId.Size = new Size(46, 15);
+            lblPeerId.TabIndex = 1;
+            lblPeerId.Text = "PeerId: ";
+            // 
+            // lblServerIP
+            // 
+            lblServerIP.AutoSize = true;
+            lblServerIP.Location = new Point(341, 125);
+            lblServerIP.Name = "lblServerIP";
+            lblServerIP.Size = new Size(35, 15);
+            lblServerIP.TabIndex = 2;
+            lblServerIP.Text = "IPv4: ";
+            // 
+            // rtbConsole
+            // 
+            rtbConsole.Location = new Point(341, 193);
+            rtbConsole.Name = "rtbConsole";
+            rtbConsole.Size = new Size(622, 537);
+            rtbConsole.TabIndex = 3;
+            rtbConsole.Text = "";
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(366, 33);
+            txtMessage.Name = "txtMessage";
+            txtMessage.Size = new Size(276, 23);
+            txtMessage.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            btnSend.Location = new Point(368, 65);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(274, 23);
+            btnSend.TabIndex = 5;
+            btnSend.Text = "SendMessage";
+            btnSend.UseVisualStyleBackColor = true;
+            btnSend.Click += btnSend_Click;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { StripServerStatus, StripPeerCount });
+            statusStrip1.Location = new Point(0, 733);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1317, 22);
+            statusStrip1.TabIndex = 6;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // StripServerStatus
+            // 
+            StripServerStatus.Name = "StripServerStatus";
+            StripServerStatus.Size = new Size(113, 17);
+            StripServerStatus.Text = "ServerStatus: Offline";
+            // 
+            // StripPeerCount
+            // 
+            StripPeerCount.Name = "StripPeerCount";
+            StripPeerCount.Size = new Size(47, 17);
+            StripPeerCount.Text = "Peers: 0";
+            // 
+            // listPeerView
+            // 
+            listPeerView.Dock = DockStyle.Left;
+            listPeerView.FormattingEnabled = true;
+            listPeerView.Location = new Point(0, 0);
+            listPeerView.Name = "listPeerView";
+            listPeerView.Size = new Size(335, 733);
+            listPeerView.TabIndex = 7;
+            // 
+            // listDiscoveredPeers
+            // 
+            listDiscoveredPeers.Dock = DockStyle.Right;
+            listDiscoveredPeers.FormattingEnabled = true;
+            listDiscoveredPeers.Location = new Point(982, 0);
+            listDiscoveredPeers.Name = "listDiscoveredPeers";
+            listDiscoveredPeers.Size = new Size(335, 733);
+            listDiscoveredPeers.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1317, 755);
+            Controls.Add(listDiscoveredPeers);
+            Controls.Add(listPeerView);
+            Controls.Add(statusStrip1);
+            Controls.Add(btnSend);
+            Controls.Add(txtMessage);
+            Controls.Add(rtbConsole);
+            Controls.Add(lblServerIP);
+            Controls.Add(lblPeerId);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label lblPeerId;
+        private Label lblServerIP;
+        private RichTextBox rtbConsole;
+        private TextBox txtMessage;
+        private Button btnSend;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel StripServerStatus;
+        private ToolStripStatusLabel StripPeerCount;
+        private ListBox listPeerView;
+        private ListBox listDiscoveredPeers;
+    }
+}
---
+++ NETConnectGUI/Form1.cs
@@ -0,0 +1,157 @@
+using NETConnect;
+using NETConnect.Peers;
+using NETConnect.Shared.Packet;
+using NETConnect.Shared.Packet.Headers;
+using System.Net;
+
+using NETConnect.MyExtensions;
+
+namespace NETConnectGUI
+{
+    public partial class Form1 : Form
+    {
+        public static Peer Self { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Self = new Peer(IPAddress.Any, 0);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //lblClientIP.Text = $"Client IP: {Self.}"
+            //lblServerIP.Text = $"Server IP: {Self.TCPServer.Address}";
+            lblPeerId.Text = $"PeerId: {Self.PeerId.ToString()}";
+
+            // GET THIS TO WORK LATER |             
+            Self.TCPServer.OnPeerConnected += (ServerClientHandle Handle, PeerTable table) =>
+            {
+                MessageBox.Show("Peer Connected");
+            };
+
+            Self.TCPServer.OnDebugMessage += (message) =>
+            {
+                MessageBox.Show($"[DEBUG] {message}");
+            };
+
+            //Self.TCPServer.OnPacketReceived += (Helper, Packet) =>
+            //{
+            //    // Handle received packet here if needed
+            //    // Example: MessageBox.Show("Packet received");
+            //    MessageBox.Show(BitConverter.ToString(Packet.GetFullPacketSpan().ToArray()));
+            //};
+
+            //Self.TCPServer.OnDataReceived += (client, header, data) =>
+            //{
+            //    // WE ONLY WANT TO SEE BROADCASTED MESSAGES
+            //    if (header.Route != PacketRoute.Broadcast) return;
+
+            //    //MessageBox.Show($"TCPServer -> {client.PacketHelper.Self.PeerId} says -> {BitConverter.ToString(data.ToArray())}");
+            //    rtbConsole.AppendText($"[TCPServer] received from {header.OriginPeerId} - [{header.Type.ToString()} {header.Action.ToString()}]\n{data.ToArray().ToUTF8String()}\n\n"); //{BitConverter.ToString(data.ToArray())}
+            //};
+
+            Self.TCPServer.OnDataReceived += HandleServerDataReceived;
+
+            //MessageBox.Show(Self.PeerId.ToString());
+
+            Task.Run(async () => await WatcherAsync());
+        }
+
+
+
+        public List<PeerTable> WiredPeers = new List<PeerTable>();
+        public async Task WatcherAsync()
+        {
+            PeriodicTimer timer = new PeriodicTimer(new TimeSpan(0, 0, 1));
+            while (!Self.TCPServer.ServerToken.IsCancellationRequested && await timer.WaitForNextTickAsync())
+            {
+                if(lblServerIP.Text != Self.TCPServer.ServerAddress)
+                {
+                    lblServerIP.Text = Self.TCPServer.ServerAddress;
+
+                    StripServerStatus.Text = "ServerStatus: Online";
+                }
+
+
+
+                var NonWiredClients = Self.ConnectedPeers.Where(x => !WiredPeers.Any(z => z.PeerId == x.PeerId));
+                if (NonWiredClients is not null && NonWiredClients?.Count() > 0)
+                {
+                    //MessageBox.Show(string.Join("\n", NonWiredClients.Select(x => $"{x.AddressPort}")));
+
+                    foreach (var peer in NonWiredClients)
+                    {
+                        peer.Client.OnDataReceived += HandleClientDataReceived;
+                        WiredPeers.Add(peer);
+                    }
+                }
+
+                // REMOVE PEERS THAT ARE NO LONGER CONNECTED
+                var InvalidPeers = WiredPeers.Where(x => !Self.ConnectedPeers.Any(z => z.PeerId == x.PeerId));
+                if (InvalidPeers is not null && InvalidPeers?.Count() > 0)
+                {
+                    //MessageBox.Show($"Invalid Peer Detected: {InvalidPeers.Count()}");
+                    foreach(var peer in InvalidPeers)
+                    {
+                        peer.Client.OnDataReceived -= HandleClientDataReceived;
+                        WiredPeers.Remove(peer);
+                    }
+
+                }
+
+                listPeerView.Invoke(new Action(() =>
+                {
+                    // Create a copy of the list to prevent "Collection was modified" errors
+                    var snapshot = WiredPeers.ToList();
+
+                    listPeerView.DataSource = snapshot;
+                    listPeerView.DisplayMember = "PeerId";
+                    listPeerView.Refresh();
+                }));
+
+                if (Self.TCPServer.MyPeerTable?.DiscoveredPeers is not null || Self.TCPServer.MyPeerTable?.DiscoveredPeers.Count() > 0)
+                {
+                    listDiscoveredPeers.Invoke(new Action(() =>
+                    {
+                        // Create a copy of the list to prevent "Collection was modified" errors
+                        //var snapshot = WiredPeers.ToList();
+
+                        listDiscoveredPeers.DataSource = Self.TCPServer.MyPeerTable.DiscoveredPeers;
+                        listDiscoveredPeers.DisplayMember = "PeerId";
+                        listDiscoveredPeers.Refresh();
+                    }));
+                }
+
+
+                StripPeerCount.Text = $"Peers: {listPeerView.Items.Count}";
+            }
+        }
+
+        private void HandleClientDataReceived(PacketHelper client, PacketHeader header, ReadOnlySpan<byte> data)
+        {
+            // WE ONLY WANT TO SEE BROADCASTED MESSAGES
+            //if (header.Route != PacketRoute.Broadcast) return;
+
+            rtbConsole.AppendText($"[TCPClient] received from {header.OriginPeerId} - [{header.Type.ToString()} {header.Action.ToString()}]\n{data.ToArray().ToUTF8String()}\n\n"); //{BitConverter.ToString(data.ToArray())}
+            rtbConsole.ScrollToCaret();
+        }
+
+        private void HandleServerDataReceived(ServerClientHandle client, PacketHeader header, ReadOnlySpan<byte> data)
+        {
+            // WE ONLY WANT TO SEE BROADCASTED MESSAGES
+            if (header.Route != PacketRoute.Broadcast) return;
+
+            //MessageBox.Show($"TCPServer -> {client.PacketHelper.Self.PeerId} says -> {BitConverter.ToString(data.ToArray())}");
+            rtbConsole.AppendText($"[TCPServer] received from {header.OriginPeerId} - [{header.Type.ToString()} {header.Action.ToString()}]\n{data.ToArray().ToUTF8String()}\n\n"); //{BitConverter.ToString(data.ToArray())}
+            rtbConsole.ScrollToCaret();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Self.Broadcast(txtMessage.Text, PacketType.Data, PacketAction.NONE);
+        }
+    }
+}
