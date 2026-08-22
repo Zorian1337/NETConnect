09d973e907c35694c062c64ea3a707b739134d03
Zorian1337 <SympathyGaming1337@gmail.com>
2026-08-22 03:54:08 -0400

    cleaning up code by; removing unused code.

--- NETConnect/ServerClientHandle.cs
+++ NETConnect/ServerClientHandle.cs
@@ -26,14 +26,6 @@ public class ServerClientHandle
 
     public HeartBeat HeartBeat { get; private set; }
 
-    //public DateTime LastPingAt { get; private set; }
-    //public DateTime PingSentAt { get; private set; }
-
-    //public int TimeoutInSecondsAfterNoPing = 10; // default 90
-    //public int SendPingInSeconds = 5; // default 30
-    //public int SendPingCoolDownInSeconds = 5; 
-
-
 
     public ServerClientHandle(Socket connection, NetworkBuffer buffers, DateTime connectedAt, ref CancellationTokenSource ClientToken)
     {
@@ -49,42 +41,4 @@ public class ServerClientHandle
     public void AddPacketHelper(ref PacketHelper Helper) => this.PacketHelper = Helper;
     public void AddHeartBeat(ref HeartBeat HeartBeat) => this.HeartBeat = HeartBeat;
 
-    //public bool TrySendPing(out bool TimedOut)
-    //{
-    //    TimedOut = false;
-    //    try
-    //    {
-    //        // Check if its time to ping
-    //        if(DateTime.UtcNow >= LastPingAt.AddSeconds(SendPingInSeconds))
-    //        {
-    //            // Cooldown to prevent ping spam if ping is missed
-    //            if(DateTime.UtcNow >= PingSentAt.AddSeconds(SendPingCoolDownInSeconds))
-    //            {
-    //                PingSentAt = DateTime.UtcNow;
-    //                var obj = new
-    //                {
-    //                    Time = DateTime.Now
-    //                };
-
-    //                string json = JsonSerializer.Serialize(obj);
-
-    //                PacketHelper.SendUTF8Packet(json, PacketActionType.Ping);
-    //            }
-    //        }
-
-    //        // Check for timeout
-    //        if (DateTime.UtcNow >= LastPingAt.AddSeconds(TimeoutInSecondsAfterNoPing))
-    //        {
-    //            TimedOut = true;
-    //            return false;
-    //        }
-
-
-    //    }
-    //    catch (Exception Ex) { Console.WriteLine($"{Ex.ToString()}"); TimedOut = true; }
-
-    //    // Check last ping vs current time to determine if its a timeout
-
-    //    return false; // False means it didnt ping
-    //}
 }
