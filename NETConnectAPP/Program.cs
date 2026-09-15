117daf32e5dcb13ce6c69c7e8f7a75fe7fe71c4f
Zorian1337 <SympathyGaming1337@gmail.com>
2026-09-14 20:20:31 -0400

    push everything else from the project to dev

--- NETConnectAPP/Program.cs
+++ NETConnectAPP/Program.cs
@@ -16,23 +16,6 @@ namespace NETConnectAPP
     {
         static async Task Main(string[] args)
         {
-            //PacketHeaderV2 header = new PacketHeaderV2();
-            //byte[] BHeader = header.ToBinaryHeader();
-            //Console.WriteLine(BitConverter.ToString(BHeader));
-            //Console.WriteLine();
-            //PacketHeaderV2 remade = PacketHeaderV2.FromBinaryHeader(BHeader);
-
-            //Console.WriteLine(remade.ToJSON(new System.Text.Json.JsonSerializerOptions() { WriteIndented = true}));
-            //return;
-
-            //RSAExport Keys = RSACrypt.CreateExport(RSAKeySize.VerySecure);
-            //string t = "this is a test";
-
-            //byte[] Encrypted = t.ToUTF8Byte().EncryptRSA(Keys.PublicKey);
-            //Console.WriteLine(Convert.ToBase64String(Encrypted));
-            //byte[] decrypted = Encrypted.DecryptRSA(Keys.PrivateKey);
-            //Console.WriteLine(decrypted);
-
 
             // SECOND BRANCH PUSH TEST!
             Console.WriteLine("C# NETConnect");
